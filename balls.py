dd95af028f515aae553a5ae049de1e5bf7231ed0
Eros Hernandez <erosh645@gmail.com>
2021-01-07 21:45:14 +0700

    Add pitch and trust

--- balls.py
+++ balls.py
@@ -2,6 +2,7 @@
 
 import sys, pygame
 import time
+import math
 pygame.init()
 
 def vec_add(a, b):
@@ -13,15 +14,19 @@ def vec_sub(a, b):
     return r
 
 
-size = width, height = 1920, 1080
+size = width, height = 1910, 900
 speed = [2.7, 0]
-thrust = [0, -1.0]
-grav = [0, 0.1]
-black = 0, 0, 0
+thrust = 1.0
+grav = [0, 0.2]
+black = (0, 0, 0)
+#bg = pygame.image.load("sn2000.png")
+bg = None
+pitch = 0
 
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(size, flags = 0, vsync = 1)
 
 ball = pygame.image.load("starship.png")
+
 
 # Halve ship size
 ballrect = ball.get_rect()
@@ -32,15 +37,25 @@ ball = pygame.transform.scale(ball, ballsize)
 
 
 while 1:
+    pitch_rad = pitch * math.pi / 180
+    thrust_vector = (-math.sin(pitch_rad) / thrust, -math.cos(pitch_rad) / thrust)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
-    if pygame.key.get_pressed()[pygame.K_UP]:
-        speed = vec_add(speed, thrust)
-    if pygame.key.get_pressed()[pygame.K_DOWN]:
-        speed = vec_sub(speed, thrust)
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        speed = vec_add(speed, thrust_vector)
+    if keys[pygame.K_DOWN]:
+        speed = vec_sub(speed, thrust_vector)
+    if  keys[pygame.K_LEFT]:
+        pitch = pitch + 2
+    if  keys[pygame.K_RIGHT]:
+        pitch = pitch - 2
         
+           
+    ball_rotated = pygame.transform.rotate(ball, pitch)        
 
     # Add gravity
     speed = vec_add(speed, grav)
@@ -51,9 +66,12 @@ while 1:
     if ballrect.top < 0 or ballrect.bottom > height:
         speed[1] = -speed[1]
 
-    screen.fill(black)
-    screen.blit(ball, ballrect)
+    if bg is None:
+        screen.fill(black)
+    else:
+        screen.blit(bg, (0, 0))
+    screen.blit(ball_rotated, ballrect)
     pygame.display.flip()
-    time.sleep(0.01)
-    print(f"speed={speed}")
+    time.sleep(0.0)
+    print(f"speed={speed} thrustvector={thrust_vector}")
 
