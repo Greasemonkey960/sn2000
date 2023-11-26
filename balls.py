1cc96ddd0154248345e9e86b78110743690312b2
Bernie Innocenti <bernie@codewiz.org>
2023-11-26 15:25:22 +0100

    Add explosion and fix various things

--- balls.py
+++ balls.py
@@ -3,6 +3,7 @@
 import sys, pygame
 import time
 import math
+
 pygame.init()
 
 def vec_add(a, b):
@@ -15,63 +16,79 @@ def vec_sub(a, b):
 
 
 size = width, height = 1910, 900
-speed = [2.7, 0]
-thrust = 1.0
-grav = [0, 0.2]
-black = (0, 0, 0)
+speed = [0.27, 0]
+thrust = 0.2
+grav = [0, 0.1]
+black = (0, 0, 0x22)
 #bg = pygame.image.load("sn2000.png")
 bg = None
 pitch = 0
 
 screen = pygame.display.set_mode(size, flags = 0, vsync = 1)
 
-ball = pygame.image.load("starship.png")
+ship = pygame.image.load("starship.png")
+explosion = pygame.image.load("nuclear-explosion.png")
+exploded = False
 
 
 # Halve ship size
-ballrect = ball.get_rect()
-ballrect[2] = ballrect[2] / 2
-ballrect[3] = ballrect[3] / 2
-ballsize = (ballrect[2], ballrect[3])
-ball = pygame.transform.scale(ball, ballsize)
+shiprect = ship.get_rect()
+shiprect[2] = shiprect[2] / 2
+shiprect[3] = shiprect[3] / 2
+ballsize = (shiprect[2], shiprect[3])
+ship = pygame.transform.scale(ship, ballsize)
+explosion = pygame.transform.scale(explosion, ballsize)
 
 
-while 1:
+while True:
     pitch_rad = pitch * math.pi / 180
-    thrust_vector = (-math.sin(pitch_rad) / thrust, -math.cos(pitch_rad) / thrust)
+    thrust_vector = (-math.sin(pitch_rad) * thrust, -math.cos(pitch_rad) * thrust)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_UP] and not exploded:
         speed = vec_add(speed, thrust_vector)
-    if keys[pygame.K_DOWN]:
-        speed = vec_sub(speed, thrust_vector)
-    if  keys[pygame.K_LEFT]:
+    #if keys[pygame.K_DOWN]:
+    #    speed = vec_sub(speed, thrust_vector)
+    if keys[pygame.K_LEFT]:
         pitch = pitch + 2
-    if  keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT]:
         pitch = pitch - 2
-        
-           
-    ball_rotated = pygame.transform.rotate(ball, pitch)        
+    if keys[pygame.K_ESCAPE]:
+        # Reset game
+        exploded = False
+        speed = [0.27, 0]
+        shiprect.left = 0
+        shiprect.top = 0
 
-    # Add gravity
-    speed = vec_add(speed, grav)
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
+    if exploded:
+        ship_rotated = explosion
+    else:
+        ship_rotated = pygame.transform.rotate(ship, pitch)
+
+        # Add gravity
+        speed = vec_add(speed, grav)
+
+    shiprect = shiprect.move(speed)
+    if shiprect.left < 0 or shiprect.right > width:
         speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
+    if shiprect.top < 0:
         speed[1] = -speed[1]
+    if shiprect.bottom > height and not exploded:
+        speed[0] = 0
+        speed[1] = 0
+        exploded = True
 
     if bg is None:
         screen.fill(black)
     else:
         screen.blit(bg, (0, 0))
-    screen.blit(ball_rotated, ballrect)
+    screen.blit(ship_rotated, shiprect)
     pygame.display.flip()
-    time.sleep(0.0)
-    print(f"speed={speed} thrustvector={thrust_vector}")
+    time.sleep(0.01)
+    print(f"speed={speed} thrust={thrust_vector}")
 
